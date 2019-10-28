d7fe2ed0821ecd74d60ba4643499ed1c2a9c9a51
EmileDeGamer <emilemolzoho@gmail.com>
2019-10-28 21:37:56 +0100

    changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
-document.getElementById('title').innerHTML = "Adventure Game"
-document.getElementById('description').innerHTML = "Welkom bij deze coole adventure game! :)"
-document.getElementById('button1').innerHTML = "Keuze 1"
-document.getElementById('button2').innerHTML = "Keuze 2"
-document.getElementById('button3').innerHTML = "Keuze 3"
+document.title = "Adventure Game"
+let title = document.getElementById('title')
+let description = document.getElementById('description')
+let button1 = document.getElementById('button1')
+let button2 = document.getElementById('button2')
+let button3 = document.getElementById('button3')
+let inventoryItem = document.getElementById('inventoryItem')
+
+button1.onclick = function(){onClickButton()}
+button2.onclick = function(){onClickButton()}
+button3.onclick = function(){onClickButton()}
+
+title.innerHTML = "Adventure Game"
+
+function onClickButton(){
+    console.log('pizza')
+}
+
+ChangeButtonText("Start", "Opties", "Stop")
+ChangeDescriptionText("Welkom bij deze coole adventure game! :)")
+ChangeInventoryItem("Knife")
+function ChangeButtonText(buttonText1, buttonText2, buttonText3){
+    button1.innerHTML = buttonText1
+    button2.innerHTML = buttonText2
+    button3.innerHTML = buttonText3
+}
+
+function ChangeDescriptionText(descText){
+    description.innerHTML = descText
+}
+
+function ChangeInventoryItem(item){
+    inventoryItem.alt = item
+    inventoryItem.src = "./images/" + item + ".png"
+    let span = document.createElement('span')
+    span.innerHTML = item
+    span.id = "inventoryItemText"
+    document.body.appendChild(span)
+}
